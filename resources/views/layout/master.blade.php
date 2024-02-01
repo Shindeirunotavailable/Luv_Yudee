899967ab9747e12ad71f4c6d9e7ba9fd23de2a30
NattakitXD <nattakit.npithanom@gmail.com>
2024-02-01 13:34:30 +0700

    up

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -20,12 +20,12 @@
          <link href="{{ asset('/assets/css/login.css') }}" rel="stylesheet" type="text/css" />
          <link href="{{ asset('/assets/css/ppcss/dashboard.css') }}" rel="stylesheet" type="text/css"/>
          <link href="{{ asset('/assets/css/propertycss/property.css') }}" rel="stylesheet" type="text/css"/>
-         {{-- <link href="{{ asset('/assets/css/hometest.css') }}" rel="stylesheet" type="text/css" /> --}}
+         <link href="{{ asset('/assets/css/hometest.css') }}" rel="stylesheet" type="text/css" />
          <link href="{{ asset('/assets/css/re-spacing.css') }}" rel="stylesheet" type="text/css" />
 </head>
 
 <body>
-    {{-- @include('layout.menu') --}}
+    @include('layout.menu')
     @yield('content')
     @include('layout.footer')
 
