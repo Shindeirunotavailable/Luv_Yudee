ce90e79841544417a2ed4bf1917a5968c5dcf0e9
Shindeirunotavailable <mmorpg.bb@gmail.com>
2024-02-07 17:54:03 +0700

    up

--- resources/views/home/content.blade.php
+++ resources/views/home/content.blade.php
@@ -80,9 +80,9 @@
                 </div>
             </div>
             <div class="card-deck text-center pt-40 pb-30 ">
-                <div class="card cards-effect background-transparent pt-30 cardBox">
+                <div class="card cards-effect bg-transparent border-0 pt-30 " >
                     <div class="text-center">
-                        <img src="https://cdn-icons-png.flaticon.com/512/185/185326.png" class="rounded-circle icon-lg icon-sm icom-xl">
+                        <img src="https://cdn-icons-png.flaticon.com/512/185/185326.png" class="icon-lg icon-sm icom-xl">
                     </div>
                     <div class="card-body mt-15">
                         <h3 class="card-title">Paris</h3>
@@ -92,9 +92,9 @@
                     <a href="javascript:void(0)" class="text-muted pb-30">
                         <span class="colorBlue">open google map. </span></a>
                 </div>
-                <div class="card cards-effect background-transparent pt-30 cardBox">
+                <div class="card cards-effect bg-transparent border-0 pt-30 " >
                     <div class="text-center">
-                        <img src="https://cdn-icons-png.flaticon.com/512/185/185326.png" class="rounded-circle icon-lg icon-sm icom-xl">
+                        <img src="https://cdn-icons-png.flaticon.com/512/185/185326.png" class="icon-lg icon-sm icom-xl">
                     </div>
                     <div class="card-body mt-15">
                         <h3 class="card-title">Paris</h3>
@@ -102,11 +102,11 @@
                         <h6 class="card-title">(315) 905-2321</h6>
                     </div>
                     <a href="javascript:void(0)" class="text-muted pb-30">
-                        <span class="colorBlue ">open google map. </span></a>
+                        <span class="colorBlue">open google map. </span></a>
                 </div>
-                <div class="card cards-effect background-transparent pt-30 cardBox">
+                <div class="card cards-effect bg-transparent border-0 pt-30 " >
                     <div class="text-center">
-                        <img src="https://cdn-icons-png.flaticon.com/512/185/185326.png" class="rounded-circle icon-lg icon-sm icom-xl">
+                        <img src="https://cdn-icons-png.flaticon.com/512/185/185326.png" class="icon-lg icon-sm icom-xl">
                     </div>
                     <div class="card-body mt-15">
                         <h3 class="card-title">Paris</h3>
@@ -121,86 +121,63 @@
     </section>
 
 
-    {{-- <section class="background-BoxIcon">
+
+
+
+      <section class="section-120">
         <div class="container">
-            <div class="row mb-20">
-                <div class="col-lg-12">
-                    <div class="text-center">
-                        <h2>Visit Our Office</h2>
-                        <p>Realton has more than 9,000 offices of all sizes and all potential of session.</p>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row mt-3" data-aos="fade-up" data-aos-delay="100">
-                <div class="col-sm-4">
-                    <div class="iconbox-style8 text-center">
-                        <div class="icon">
-                            <img src="https://cdn-icons-png.flaticon.com/512/185/185326.png" width="40%" height="50%">
-                        </div>
-                        <div class="iconbox-content">
-                            <h4 class="title">Paris</h4>
-                            <p class="text mb-1">1301 2nd Ave, Seattle, WA 98101</p>
-                            <h6 class="mb10">(315) 905-2321</h6><a class="text-decoration-underline" href="#">Open
-                                Google Map</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="iconbox-style8 text-center">
-                        <div class="icon">
-                            <img src="https://static.thenounproject.com/png/164722-200.png" width="40%" height="50%">
-                        </div>
-                        <div class="iconbox-content">
-                            <h4 class="title">London</h4>
-                            <p class="text mb-1">1301 2nd Ave, Seattle, WA 98101</p>
-                            <h6 class="mb10">(315) 905-2321</h6><a class="text-decoration-underline" href="#">Open
-                                Google Map</a>
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="iconbox-style8 text-center">
-                        <div class="icon">
-                            <img src="https://cdn3.iconfinder.com/data/icons/famous-city-skyline/48/Sed-12-512.png"
-                                width="40%" height="50%">
-                        </div>
-                        <div class="iconbox-content">
-                            <h4 class="title">New York</h4>
-                            <p class="text mb-1">1301 2nd Ave, Seattle, WA 98101</p>
-                            <h6 class="mb10">(315) 905-2321</h6>
-                            <a class="text-decoration-underline" href="#">Open Google Map</a>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section> --}}
-
-    {{-- <section class="our-cta pt0">
-        <div class="cta-banner mx-auto maxw1600 pt120 pt60-md pb120 pb60-md bdrs12 position-relative mx20-lg mt-3">
-            <div class="container">
-                <div class="row align-items-center">
-                    <div class="col-lg-7 col-xl-6 aos-init aos-animate" data-aos="fade-right">
-                        <div class="cta-style1">
+            <div class="card background-grey border-0 h-250">
+                <div class="card-body">
+                    <div class="row pt-60 ">
+                        <div class="col-lg-6">
                             <h2 class="cta-title">Need help? Talk to our expert.</h2>
-                            <p class="cta-text mb-0">Talk to our experts or Browse through more properties.</p>
+                            <p class="cta-text">Talk to our experts or Browse through more properties.</p>
                         </div>
-                    </div>
-                    <div class="col-lg-5 col-xl-6 aos-init aos-animate" data-aos="fade-left">
-                        <div class="cta-btns-style1 d-block d-sm-flex align-items-center justify-content-lg-end">
-                            <a class="ud-btn btn-transparent mr30 mr0-xs" href="/conract" onclick="scrollToTop()">
-                                Contact Us <i class="fa fa-long-arrow-right" aria-hidden="true"></i></a>
-                            <a class="ud-btn btn-dark Blondie" href="Tel:+66915453939">
-                                <span class="fa fa-phone vam pe-2"></span>091-545-3939</a>
+                        <div class="col-lg-6">
+                            <div class="d-flex justify-content-end pt-20">
+                                <button type="button" class="btn btn-primary afterButton" onclick="scrollToTop()">
+                                    <i class="fa fa-long-arrow-right" aria-hidden="true"></i> Contact Us  </button>
+
+                                <button type="button" class="btn btn-primary afterButton" href="Tel:0915453939">
+                                    <i class="fa fa-phone vam pe-2" aria-hidden="true"></i>091-545-3939</button>                                
+                            </div>
                         </div>
                     </div>
                 </div>
+              </div>
+        </div>
+      </section>
+
+    {{-- <section class="section-120">
+        <div class="background-grey h-70">
+
+            <div class="container ">
+
+                <div class="row">
+                    <div class="col-lg-6">
+
+                            <h2 class="cta-title">Need help? Talk to our expert.</h2>
+                            <p class="cta-text">Talk to our experts or Browse through more properties.</p>
+
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="d-flex justify-content-end"> 
+                                    <button type="button" class="btn btn-primary afterButton" onclick="scrollToTop()">
+                                    <i class="fa fa-long-arrow-right" aria-hidden="true"></i> Contact Us  </button>
+                                    <button type="button" class="btn btn-primary afterButton" href="Tel:0915453939">
+                                    <i class="fa fa-phone vam pe-2" aria-hidden="true"></i>091-545-3939</button>
+                        </div>
+
+                    </div>
+                </div>
             </div>
+
         </div>
     </section> --}}
+
     {{-- 
     <div class="scrollToHome" style="cursor: pointer;">
         <i class="fas fa-angle-up"></i>
     </div> --}}
+
 @endsection
