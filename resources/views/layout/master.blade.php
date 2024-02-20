137f8c7cdaf31df0070a69ecf729c7b78afc5387
NattakitXD <nattakit.npithanom@gmail.com>
2024-02-20 09:02:13 +0700

    up

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -48,7 +48,7 @@
     @yield('content')
     {{-- @include('layout.footer') --}}
 
-    
+
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
     <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
