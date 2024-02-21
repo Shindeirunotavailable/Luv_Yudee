a5d4c30f301a2dea1b57cafee117832715611b03
Redmagics <thanabodeetonwong9@gmail.com>
2024-02-21 15:35:49 +0700

    pod

---
+++ resources/views/home/boxStores.blade.php
@@ -0,0 +1,42 @@
+<section class="pt-45 pb-0">
+    <div class="container max-width1600 bdb1 pb-50">
+        <div class="row justify-content-center wow ">
+            <div class="col-sm-6 col-lg-4 col-xl-2">
+                <div class="stores_one text-center">
+                    <div class="details">
+                        <ul class="pl-0 mb-0">
+                            <li>
+                                <div class="timer"><span><span>400</span></span></div>
+                            </li>
+                        </ul>
+                        <p class="text mb-0">Stores around the world</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4 col-xl-2">
+                <div class="stores_one text-center">
+                    <div class="details">
+                        <ul class="pl-0 mb-0">
+                            <li>
+                                <div class="timer"><span><span>200+</span></span></div>
+                            </li>
+                        </ul>
+                        <p class="text mb-0">Stores around the world</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-6 col-lg-4 col-xl-2">
+                <div class="stores_one text-center">
+                    <div class="details">
+                        <ul class="pl-0 mb-0">
+                            <li>
+                                <div class="timer"><span><span>1k+</span></span></div>
+                            </li>
+                        </ul>
+                        <p class="text mb-0">Stores around the world</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
