8d61a81c90b483dff398be9fca84198fea486064
Redmagics <thanabodeetonwong9@gmail.com>
2024-03-21 10:36:50 +0700

    pod

--- resources/views/dashboard/review.blade.php
+++ resources/views/dashboard/review.blade.php
@@ -17,7 +17,7 @@
                                 <div class="col-lg-12">
                                     <div
                                         class="total_review d-sm-flex align-items-center justify-content-between mb-d-20 mt60">
-                                        <h6 class="fw-600 fs-17 mb15"><i class="fas fa-star fs-12 pe-2"></i>5.0 · 3 reviews</h6>
+                                        <h6 class="fw-600 fs-17 mb15">reviews</h6>
                                         <div
                                             class="page_control_shorting d-flex align-items-center justify-content-center justify-content-sm-end">
                                             <div class="pcs_dropdown mb15 d-flex align-items-center"><span
@@ -34,7 +34,6 @@
 
 
                                 <div class="col-md-12">
-
                                     <div class="media border-top pt-7 pb-6 d-sm-flex d-block text-sm-left text-center ">
                                         <img src="{{ asset('/assets/images/review-1.jpg') }}"
                                             alt="Danny Fox" class=" review-icon mr-sm-8 mb-sm-0 img-fluid"
@@ -320,92 +319,4 @@
         </div>
     </div>
     <div class="footer100vh"></div>
-{{--
-    <footer class="mb-1 pt-30 m-0 mt-footer ">
-        <section class="footer-style1 at-home4 pt-60 pb-0 bdrs-12">
-            <div class="container">
-                <div class="row">
-                    <div class="col-sm-6 col-lg-3">
-                        <div class="footer-widget light-style mb-4 mb-lg-5">
-                            <a class="footer-logo"href="{{url("home")}}">
-                                <img class="mb-10" src="{{ asset('/assets/images/YuuDee2-logo.png') }}" alt="YuuDee2-logo.png"  />
-                            </a>
-
-                            <div class="row ml-1 mb-4 mb-lg-5">
-                                <div class="contact-info w-100">
-                                    <p class="text mb-0">58 Howard Street #2 San Francisco</p>
-                                </div>
-                                <div class="contact-info w-100">
-                                    <p class="text mb-0">hi@homez.com</p>
-                                </div>
-                                <div class="contact-info w-100">
-                                    <p class="text mb-0">+(0) 123 050 945 02</p>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="col-sm-6 col-lg-3">
-                        <div class="footer-widget mb-4 mb-lg-5 ps-0 ps-lg-5">
-                            <div class="link-style1 light-style">
-                                <h6 class="fw-700">Popular Search</h6>
-                                <ul class="link-list ps-0 pl-0">
-                                    <li><a href="#">Apartment for Rent</a></li>
-                                    <li><a href="#">Apartment Low to Hide</a></li>
-                                    <li><a href="#">Offices for Buy</a></li>
-                                    <li><a href="#">Offices for Rent</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-3">
-                        <div class="footer-widget mb-4 mb-lg-5 ps-0 ps-lg-5">
-                            <div class="link-style1 light-style mb-3">
-                                <h6 class="fw-700">Quick Links</h6>
-                                <ul class="ps-0 pl-0">
-                                    <li><a href="#">Terms of Use</a></li>
-                                    <li><a href="#">Privacy Policy</a></li>
-                                    <li><a href="#">Pricing Plans</a></li>
-                                    <li><a href="#">Our Services</a></li>
-                                    <li><a href="#">Contact Support</a></li>
-                                    <li><a href="#">Careers</a></li>
-                                    <li><a href="#">FAQs</a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-sm-6 col-lg-3">
-                        <div class="footer-widget mb-4 mb-lg-5">
-                            <div class="mailchimp-widget mb-30">
-                                <form action="{{ url('/home_email') }}"  method="POST" class="needs-validation" id="home_email">
-                                    @csrf
-                                    <h6 class="title mb-30 fw-700">Sign Up for Our Newsletter</h6>
-                                    <div class="mailchimp-style1 at-home4 white-version">
-                                        <input type="email" id="" class="form-control" placeholder="Your Email" name="email">
-
-
-                                        <button type="submit" class="g-recaptcha"
-                                            data-sitekey="{{config('services.recaptcha.site_key')}}" data-callback='onSubmitemail' data-action='home'>
-                                            <i class="fa-solid fa-rocket"></i>
-                                        </button>
-                                    </div>
-
-                                    <div class="colorRed" id="ShowErrorEmail"></div>
-                                </form>
-                                <div class="social-style1 light-style py-2">
-                                    <a href="#"><i class="fab fa-facebook-f list-inline-item"></i></a>
-                                    <a href="#"><i class="fab fa-twitter list-inline-item"></i></a>
-                                    <a href="#"><i class="fab fa-instagram list-inline-item"></i></a>
-                                    <a href="#"><i class="fab fa-linkedin-in list-inline-item"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-        </section>
-
-    </footer> --}}
 </div>
